74c84e15988d924eafa3a443d40c9c8900a566f6
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-21 19:08:33 +0200

    Métodos clase Alumno (hija). Herencia de los públicos de Persona.

--- POO - Ejercicios y ejemplos/05 - Herencia. Clases Persona y alumno/Alumno.cpp
+++ POO - Ejercicios y ejemplos/05 - Herencia. Clases Persona y alumno/Alumno.cpp
@@ -4,16 +4,20 @@ Alumno::Alumno(string nomb, int _edad, float _nota, int _nummat):Persona (nomb,_
 	nummat = _nummat;
 	notafinal = _nota;
 }
+/*Declaro todos los atributos que necesita el constructor, pero el nombre son 
+pertenecientes a la clase persona. Lo indico al final.*/
 
 Alumno::~Alumno() {
-
-}
+} //El destructor sigue estando vacío. 
 
 void Alumno::mostrarAlumno() {
 	mostrarPersona();
-	/*tengo acceso al metodo mostrarPersona, perteneciente a la clase
-	Persona, porque la clase alumno es de tipo public Persona */
-
+	/*Tengo acceso al metodo mostrarPersona, perteneciente a la clase
+	Persona, porque la clase alumno es de tipo public Persona (tiene 
+ 	acceso a los métodos Puúblicos de la clase Persona.*/
+	
+	/* La función mostrarPersona() muestra el nombre y edad. Aún me falta
+	mostrar los atributos "nota final" y "numero de matrícula" */
 	cout << "El numero de matricula del alumno es: " << nummat << endl;
 	cout << "Y su nota final es: " << notafinal << endl;
 
