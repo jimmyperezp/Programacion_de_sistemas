83d7402207e545e6f634a14057baab30f7f87318
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-21 18:49:05 +0200

    Actualizado main. Comentado uso de get y set.

--- POO - Ejercicios y ejemplos/04 - Intro getters y setters. Clase punto/main.cpp
+++ POO - Ejercicios y ejemplos/04 - Intro getters y setters. Clase punto/main.cpp
@@ -2,13 +2,15 @@
 
 int main() {
 
-	Punto punto1;
+	Punto punto1; //Creo un objeto de clase Punto, pero no le paso nada al constructor.
 
-	punto1.setPunto(15, 10); //acabo de darle valores a x e y desde el main
+	punto1.setPunto(15, 10); //le doy valores usando el set.
+
+	//Y puedo mostrar los valores usando los métodos get:
 	printf(" El valor de la coordenada x es %d\n", punto1.getPuntoX());
 	printf("El valor de la coordenada y es %d\n", punto1.getPuntoY());
 
-	punto1.~Punto();
+	punto1.~Punto(); //Una vez he terminado, "destruyo" el objeto.
 
 	system("pause");
 	return 0;
