63b10050c3c5555330dee70d6554807e3f5db06a
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-21 15:47:04 +0200

    Métodos de la clase rectángulo explicados.

--- POO - Ejercicios y ejemplos/Creacion de clases. Rectangulo/rectangulo.cpp
+++ POO - Ejercicios y ejemplos/Creacion de clases. Rectangulo/rectangulo.cpp
@@ -1,13 +1,18 @@
 #include "rectangulo.h"
 
+// 1. CONSTRUCTOR: 
+// Tiene el mismo nombre de la clase.
+// La sintaxis es: Clase::Constructor(parámetros del constructor)
 Rectangulo::Rectangulo(float l, float a) {
-	largo = l;
+
+	// Le pongo un nombre distinto a los parámetros recibidos para evitar líos.
+	largo = l;  //Asigno los parámetros recibidos a las propiedades de la clase.
 	ancho = a;
 
 }
 
 void Rectangulo::perimetro(){
-	float perim;
+	float perim; //Me creo una variable que usaré para el cálculo.
 	perim = 2 * largo + 2 * ancho;
 	printf("El perimetro del rectangulo es %.2f metros.\n",perim);
 
@@ -16,7 +21,7 @@ void Rectangulo::perimetro(){
 
 void Rectangulo::area() {
 
-	float are;
+	float are; // Igual que antes, llamo a la variable de manera distinta que el método, para evitar conflictos.
 	are = largo * ancho;
 
 	printf("El area del rectangulo es %.2f metros cuadrados\n", are);
