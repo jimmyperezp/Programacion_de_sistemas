d92a8957397592be51d378caf636cae8bc0cfe3c
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-21 19:04:31 +0200

    Definición clase HIJA -> Alumno.

--- POO - Ejercicios y ejemplos/05 - Herencia. Clases Persona y alumno/Alumno.h
+++ POO - Ejercicios y ejemplos/05 - Herencia. Clases Persona y alumno/Alumno.h
@@ -1,23 +1,26 @@
 #pragma once
 #include "Persona.h"
-class Alumno : public Persona    
-	/*la clase alumno es una clase "hija" de persona
-	el public significa que puedo acceder a toods los atributos y
-	metodos publicos de la clase padre (Persona) */ 
-{
-private:
-	float notafinal;
-	int nummat;
 
-public:
+class Alumno : public Persona {
+/*La clase alumno es una clase "hija" de persona. El "public" significa
+que puedo acceder a todos los atributos y métodos que sean públicos de la 
+clase Padre (Persona)*/ 
 
-	Alumno(string, int, float, int);  
-	//tiene los dos atributos de alumno y los dos de persona
-	//Primero los atributos de Persona, luego los de alumno
+	private:
+		float notafinal;
+		int numero_matricula;
 	
-	~Alumno();
-	void mostrarAlumno();
-
-
+	public:
+	
+		Alumno(string, int, float, int);  
+		/* No solo tiene los métodos propios, sino que tiene también los pertenecientes 
+		a la clase Padre*/
+		/*Pero, ¿En qué orden?: 
+  		Hay que respetar los rangos. Primero van los de la clase padre. Estos eran 
+		el nombre (string) y la edad (int). Después vienen los propios al alumno,
+  		notafinal(float) y el número de matrícula (int)*/
+		
+		~Alumno();
+		void mostrarAlumno();
+	
 };
-
