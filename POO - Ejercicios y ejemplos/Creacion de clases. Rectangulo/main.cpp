140436841e32a8b7ec39a1f2448f9556b0c91393
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-21 15:50:18 +0200

    Main de clase rectángulo explicada.

--- POO - Ejercicios y ejemplos/Creacion de clases. Rectangulo/main.cpp
+++ POO - Ejercicios y ejemplos/Creacion de clases. Rectangulo/main.cpp
@@ -1,19 +1,21 @@
-/* Ejercicio 1: construya una clase llamada rectangulo que tenga los siguientes atributos :
-
+/* Ejercicio 1: 
+----------------
+construya una clase llamada rectangulo que tenga los siguientes atributos :
 largo y ancho, y los siguientes metodos: perimetro () y area()*/
 
 #include <iostream>
-#include "rectangulo.h"
+#include "rectangulo.h"  //-> Ya he visto que aquí he declarado atributos y métodos.
 using namespace std;
 
 int main() {
 	
-	Rectangulo r1 = Rectangulo(2, 3);
-	r1.perimetro();
-	r1.area();
-
+	Rectangulo r1 = Rectangulo(2, 3); //Le paso al constructor el largo y el ancho
+	// r1 es un objeto de la clase rectángulo. 
 	
-	system("pause");
+	r1.perimetro();  // Accedo al método perímetro
+	r1.area();		 // Accedo al método área.
+
+		system("pause");
 	return 0;
 
 
