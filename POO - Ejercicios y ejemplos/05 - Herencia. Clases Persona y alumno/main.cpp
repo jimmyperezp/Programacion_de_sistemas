65d6f0102e379713d40e612068cdacbfce8e2149
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-21 19:10:12 +0200

    Main. Creación de un alumno.

--- POO - Ejercicios y ejemplos/05 - Herencia. Clases Persona y alumno/main.cpp
+++ POO - Ejercicios y ejemplos/05 - Herencia. Clases Persona y alumno/main.cpp
@@ -1,20 +1,15 @@
-
 #include "Alumno.h"
 
 int main() {
 
-
 	Alumno alumno1("Jimmy", 21, 8.75, 20259);
 
 	/*Como ya vi en el constructor de alumno, tengo que pasarle :
-	 nombre,edad, codigo del alumno, nota final. 
-	 El nombre y la edad son los atributos de la clase persona. */
+	nombre, edad, nota final y número de matrícula.
+	El nombre y la edad son los atributos de la clase persona. */
 
 	alumno1.mostrarAlumno();
 
 	system("pause");
 	return 0;
-
 }
-
-
