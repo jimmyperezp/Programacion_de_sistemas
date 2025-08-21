69d321d46a9754f9fe3f0d1f432ee2c738ab216b
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-21 15:42:44 +0200

    Añadidos comentarios.

--- POO - Ejercicios y ejemplos/Creacion de clases. Rectangulo/rectangulo.h
+++ POO - Ejercicios y ejemplos/Creacion de clases. Rectangulo/rectangulo.h
@@ -3,12 +3,13 @@
 
 class Rectangulo
 {
-private:
+private: //Atributos -> Privados. Solo los puedo modificar desde los métodos.
 	float largo;
 	float ancho;
 
-public:
-	Rectangulo(float,float);
+public: // Métodos de la clase.
+	Rectangulo(float,float); //Es el constructor.
+							//Recibe dos floats para inicializar la clase (largo y ancho)
 	void perimetro ();
 	void area ();
 
