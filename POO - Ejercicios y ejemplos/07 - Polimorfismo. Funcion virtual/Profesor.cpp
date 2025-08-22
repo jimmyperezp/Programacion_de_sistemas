9c135cc410a8cc57d8e03b0f6f2772e11f0671bc
Jimmy <jaimeperezzp@gmail.com>
2025-08-22 12:10:39 +0200

    Métodos clase hija Profesor. Definición de método polimórfico Mostrar

--- POO - Ejercicios y ejemplos/07 - Polimorfismo. Funcion virtual/Profesor.cpp
+++ POO - Ejercicios y ejemplos/07 - Polimorfismo. Funcion virtual/Profesor.cpp
@@ -1,6 +1,6 @@
 #include "Profesor.h"
 
-Profesor::Profesor(string _nombre, int _edad, string _asignatura) : Persona(_nombre, _edad) {
+Profesor::Profesor(string _nombre, int _edad, string _asignatura) : Persona(_nombre, _edad){
 
 	asignatura = _asignatura;
 
@@ -8,10 +8,11 @@ Profesor::Profesor(string _nombre, int _edad, string _asignatura) : Persona(_nom
 
 Profesor::~Profesor(){}
 
-
 void Profesor::mostrar() {
+
 	Persona::mostrar();
+	//Lo primero que hago es llamar al método mostrar de Persona.
+	// Para hacerlo, indico la clase y después el método virtual
+	
 	cout << "El profesor imparte la asignatura: " << asignatura << endl;
-
 }
-
