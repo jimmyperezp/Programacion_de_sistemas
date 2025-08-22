b1279b88427104a7f5e5233dadacc80949c99603
Jimmy <jaimeperezzp@gmail.com>
2025-08-22 12:19:22 +0200

    Llamada a dos constructores distintos

--- POO - Ejercicios y ejemplos/02 - Sobrecarga Constructores. Clase fecha/main.cpp
+++ POO - Ejercicios y ejemplos/02 - Sobrecarga Constructores. Clase fecha/main.cpp
@@ -7,8 +7,7 @@ int main() {
 	Fecha hoy(4, 7, 2023); //Opción 1 --> Le mando los parámetros separados.
 	Fecha ayer(20230703); // Opción 2 --> Mando un único número: AAAA/MM/DD
 	
-	/*Además, he usado la sintaxis del cosntructor directamente. También hubiera
- 	sido válido declararlo como: 
+	/*Además, he usado la sintaxis del cosntructor directamente. También hubiera sido válido declararlo como: 
   	Fecha hoy = Fecha(4,7,2023)*/
 
 	hoy.mostrarFecha();
