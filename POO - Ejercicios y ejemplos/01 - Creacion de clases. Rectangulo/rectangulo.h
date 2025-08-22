9a6541c6c5e1879e30fdba006cb399242c05ead2
Jimmy <jaimeperezzp@gmail.com>
2025-08-22 12:17:16 +0200

    Definiciones Clase rectángulo. Atributos (privados) y métodos (publicos). Uso del constructor.

--- POO - Ejercicios y ejemplos/01 - Creacion de clases. Rectangulo/rectangulo.h
+++ POO - Ejercicios y ejemplos/01 - Creacion de clases. Rectangulo/rectangulo.h
@@ -1,17 +1,15 @@
 #pragma once
 #include <stdio.h>
 
-class Rectangulo
-{
-private: //Atributos -> Privados. Solo los puedo modificar desde los métodos.
-	float largo;
-	float ancho;
+class Rectangulo{
 
-public: // Métodos de la clase.
-	Rectangulo(float,float); //Es el constructor.
-							//Recibe dos floats para inicializar la clase (largo y ancho)
-	void perimetro ();
-	void area ();
+	private: //Atributos -> Privados. Solo los puedo modificar desde los métodos.
+		float largo;
+		float ancho;
+
+	public: // Métodos de la clase.
+		Rectangulo(float,float); //Es el constructor. Recibe dos floats para inicializar la clase (largo y ancho)
+		void perimetro ();
+		void area ();
 
 };
-
