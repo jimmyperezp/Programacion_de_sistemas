e2979fb85688136fcd9dc76a40b4805a1163af25
Jaime Pérez <jaimeperezzp@gmail.com>
2025-08-22 13:07:15 +0200

    Clase Zoo. Composición con Felinos.

--- POO - Ejercicios y ejemplos/09 - Ejercicio Faunia. Clases Zoo, felinos y animales/Codigo/Zoo.h
+++ POO - Ejercicios y ejemplos/09 - Ejercicio Faunia. Clases Zoo, felinos y animales/Codigo/Zoo.h
@@ -1,12 +1,12 @@
 #include "Felinos.h"
 
 class Zoo {
-private:
-	string nombre;
-	int precio;
-	Felinos losFelinos;
-public:
-	Zoo(string, int, int, string, string);
-
-	Felinos getFelinos();
+	private:
+		string nombre;
+		int precio;
+		Felinos losFelinos;
+	public:
+		Zoo(string, int, int, string, string);
+	
+		Felinos getFelinos();
 };
