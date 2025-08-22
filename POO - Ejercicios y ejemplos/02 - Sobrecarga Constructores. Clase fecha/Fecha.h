c91bee5d2aa5374cd1a669da372d8eb835bd928e
Jimmy <jaimeperezzp@gmail.com>
2025-08-22 12:19:52 +0200

    Definición clase con 2 constructores distintos

--- POO - Ejercicios y ejemplos/02 - Sobrecarga Constructores. Clase fecha/Fecha.h
+++ POO - Ejercicios y ejemplos/02 - Sobrecarga Constructores. Clase fecha/Fecha.h
@@ -1,22 +1,21 @@
 #pragma once
 #include "cabecera.h"
 
-class Fecha
-{
-private:
-	int dia,mes,ano;
+class Fecha{
 
-public:
-/*Imaginemos que se puede indicar la fecha de dos maneras. Si la forma utilizada no coincide con los parámetros
-o los tipos que espera el constructor, entonces habrá un problema. Esto lo soluciono con múltiples constructores*/
+	private:
+		int dia,mes,ano;
 
-	Fecha(int, int, int); 
-	//Este es el primer constructor. Recibiendo dia mes y año por separado 
+	public:
+	/*Imaginemos que se puede indicar la fecha de dos maneras. Si la forma utilizada no coincide con los parámetros
+	o los tipos que espera el constructor, entonces habrá un problema. Esto lo soluciono con múltiples constructores*/
 
-	Fecha(long); 
-	/* este es el segundo constructor. Recibe un unico entero largo, de la forma
-	20230704 (año dia mes) todo seguido,el cual tendre que "desglosar" */
+		Fecha(int, int, int); 
+		//Este es el primer constructor. Recibiendo dia mes y año por separado 
 
-	void mostrarFecha();
+		Fecha(long); 
+		/* este es el segundo constructor. Recibe un unico entero largo, de la forma
+		20230704 (año dia mes) todo seguido,el cual tendre que "desglosar" */
+
+		void mostrarFecha();
 };
-
